6dcb2039a9d6c97b294886e95b2287ae818adb9e
walkP <wpeters@ualberta.ca>
2019-03-16 15:31:00 -0600

    more blockwin tests

--- blockwin_test.py
+++ blockwin_test.py
@@ -33,3 +33,23 @@ play b G7
 play w F2
 policy_moves
 #?[Blockwin D2]
+
+clear_board
+play b A1
+play w B1
+play b G1
+play w C1
+play b A7
+play w E1
+play b G7
+play w F1
+play b A2
+play w B7
+play b G2
+play w C6
+play b A6
+play w D5
+play b G6
+play w E4
+policy_moves
+#?[Blockwin D1 F3]
